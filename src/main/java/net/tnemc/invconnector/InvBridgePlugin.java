bfd16978fba8abdf2a02bf693797ab46ddc92390
creatorfromhell <daniel.viddy@gmail.com>
2023-09-03 19:28:26 -0400

    Initial Commit.

---
+++ src/main/java/net/tnemc/invconnector/InvBridgePlugin.java
@@ -0,0 +1,43 @@
+package main.java.net.tnemc.invconnector;
+/*
+ * The New Kings
+ * Copyright (C) 2022 - 2023 Daniel "creatorfromhell" Vidmar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * InvBridgePlugin
+ *
+ * @author creatorfromhell
+ * @since 0.0.1.0
+ */
+public class InvBridgePlugin extends JavaPlugin {
+
+
+  @Override
+  public void onEnable() {
+
+    if(Bukkit.getPluginManager().isPluginEnabled("InvSeePlusPlus")) {
+
+    }
+
+    if(Bukkit.getPluginManager().isPluginEnabled("OpenInv")) {
+
+    }
+  }
+}
